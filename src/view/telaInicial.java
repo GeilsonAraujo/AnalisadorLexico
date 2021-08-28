5ac5acc9e62b1f35ffd2e4f1216d91ab3e901757
GeilsonArujo <geilsonopovo@gmail.com>
2021-08-28 13:42:28 -0300

    adicionando arquivos do programa

---
+++ src/view/telaInicial.java
@@ -0,0 +1,101 @@
+package view;
+
+import exception.IsiLexicalException;
+import Isi.IsiParser;
+import Isi.IsiScanner;
+import exception.IsiSyntaxException;
+import javax.swing.JOptionPane;
+
+public class telaInicial extends javax.swing.JFrame {
+
+    public telaInicial() {
+        initComponents();
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Vamos Lá");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jScrollPane2.setViewportView(jTextArea2);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 449, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        jTextArea2.setText("");
+        try {
+            
+                IsiScanner sc = new IsiScanner("arquivoInicial.txt");
+                IsiParser  pa = new IsiParser(sc);          
+                pa.E();
+                
+        }
+        catch (IsiLexicalException ex) {
+                        jTextArea2.setText("Lexical Erro: " + ex.getMessage());			
+		}
+        
+		catch (IsiSyntaxException ex) {
+			jTextArea2.setText("Syntax Erro: " + ex.getMessage());
+		}
+		catch (Exception ex) {
+			jTextArea2.setText("Generic Erro: ");
+                        //jTextArea2.setText(ex.getClass()+ ex.getMessage());
+                        JOptionPane.showMessageDialog(null, "Erro na abertura dos arquivo: " + ex.getMessage());
+		}
+        
+            
+    }//GEN-LAST:event_jButton1ActionPerformed
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
+    // End of variables declaration//GEN-END:variables
+}
